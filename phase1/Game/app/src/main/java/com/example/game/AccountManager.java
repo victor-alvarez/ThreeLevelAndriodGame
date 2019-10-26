635c0ef7daa6a7c32f6b0638847d0f86fe270bb6
Clifton <clifton.sahota@mail.utoronto.ca>
2019-10-26 17:00:53 -0400

    "Created basic layout for AccountManager class."

--- phase1/Game/app/src/main/java/com/example/game/AccountManager.java
+++ phase1/Game/app/src/main/java/com/example/game/AccountManager.java
@@ -1,4 +1,58 @@
 package com.example.game;
 
-public class AccountManager {
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Optional;
+
+/**
+ * Account manager that manages accounts.
+ */
+public class AccountManager implements Serializable {
+
+    /**
+     * List of accounts.
+     */
+    ArrayList<Account> accounts;
+
+    /**
+     * Create an account manager
+     */
+    AccountManager(){
+        //TODO: Read data from a file
+    }
+
+    /**
+     * Get account with name same as user id.
+     * @return the account with matching name;
+     */
+    Account getAccount(String name){
+        for (Account account : accounts){
+            if (account.getUser_id().equals(name)){
+                return account;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Adds a new account to the arraylist. Also updates the data file.
+     */
+     void addAccount(String name){
+        accounts.add(new Account(name));
+        writeToFile();
+    }
+
+    /**
+     * Writes data about the accounts to a file
+     */
+    void writeToFile(){
+        return;
+    }
+
+    /**
+     * Reads data about the accounts to a file.
+     */
+    private void readFromFile(){
+        return;
+    }
 }
