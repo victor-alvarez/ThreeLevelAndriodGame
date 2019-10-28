79e8d411a2fa69688dbcc2982edfb1429f1823d0
Victor <victor.alvarez@mail.utoronto.ca>
2019-10-27 21:29:16 -0400

    "Cleaned up some unnecessary code and made Game1Activity.java easier to read and understand."

--- phase1/Game/app/src/main/java/com/example/game/Game1Activity.java
+++ phase1/Game/app/src/main/java/com/example/game/Game1Activity.java
@@ -5,27 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
 public class Game1Activity extends AppCompatActivity {
 
-    Button button;
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_game1);
+  }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game1);
-
-        button = findViewById(R.id.game1Button);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                nextGame();
-            }
-        });
-    }
-
-    public void nextGame() {
-        Intent intent = new Intent(this, Game2Activity.class);
-        startActivity(intent);
-    }
+  public void nextGame(View view) {
+    Intent intent = new Intent(this, Game2Activity.class);
+    startActivity(intent);
+  }
 }
