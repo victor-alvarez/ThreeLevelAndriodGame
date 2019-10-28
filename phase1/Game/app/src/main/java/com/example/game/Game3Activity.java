2038e5b745268d5e6c23c95edd760e05d86bc83e
Victor <victor.alvarez@mail.utoronto.ca>
2019-10-28 19:03:06 -0400

    "Cleaned up code in Game2Activity.java and Game3Activity.java in the exact same way I cleaned up code in Game1Activity.java"

--- phase1/Game/app/src/main/java/com/example/game/Game3Activity.java
+++ phase1/Game/app/src/main/java/com/example/game/Game3Activity.java
@@ -5,26 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
 public class Game3Activity extends AppCompatActivity {
-
-    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game3);
-
-        button = findViewById(R.id.game3Button);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                nextGame();
-            }
-        });
     }
 
-    public void nextGame() {
+    public void nextGame(View view) {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
