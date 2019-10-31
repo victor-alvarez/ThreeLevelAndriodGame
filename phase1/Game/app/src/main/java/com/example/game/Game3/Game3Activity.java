f1f0d91f9478e892eef34024fc40c49d30f67cf8
Chirag Rana <chirag.rana@mail.utoronto.ca>
2019-10-30 20:51:18 -0400

    "Created a Game3ViewActivity, where Game3 will be played."

--- phase1/Game/app/src/main/java/com/example/game/Game3/Game3Activity.java
+++ phase1/Game/app/src/main/java/com/example/game/Game3/Game3Activity.java
@@ -12,18 +12,28 @@ import com.example.game.R;
 
 public class Game3Activity extends AppCompatActivity {
 
-  Account account;
+    Account account;
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_game3);
-    account = (Account) getIntent().getSerializableExtra("ac");
-  }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game3);
+        account = (Account) getIntent().getSerializableExtra("ac");
+    }
 
-  /** Called when the user taps the "To Main Menu" button */
-  public void toMainMenu(View view) {
-    Intent intent = new Intent(this, MainActivity.class);
-    startActivity(intent);
-  }
+    /**
+     * Called when the user taps the "To Main Menu" button
+     */
+    public void toMainMenu(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    /**
+     * Called when user taps the "Play" Button.
+     * */
+    public void toPlay(View view) {
+        Intent intent = new Intent(this, Game3PlayActivity.class);
+        startActivity(intent);
+    }
 }
