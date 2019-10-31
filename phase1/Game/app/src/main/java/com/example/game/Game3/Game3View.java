50a514777270dbb9962659525642e36a81e5205e
Chirag Rana <chirag.rana@mail.utoronto.ca>
2019-10-31 17:29:07 -0400

    "Created game loop structure for Game 3."

--- phase1/Game/app/src/main/java/com/example/game/Game3/Game3View.java
+++ phase1/Game/app/src/main/java/com/example/game/Game3/Game3View.java
@@ -5,13 +5,52 @@ import android.view.SurfaceView;
 
 public class Game3View extends SurfaceView implements Runnable {
 
+    private boolean isPlaying = true;
+    private Thread thread;
 
     public Game3View(Context context) {
         super(context);
     }
 
+    /**
+     * Runs the thread.
+     */
     @Override
     public void run() {
+        while (isPlaying) {
+            update();
+            draw();
+            sleep();
+            checkGameEnded();
+        }
+    }
+
+    /**
+     * Checks if game is done.
+     */
+    private void checkGameEnded() {
+
+    }
+
+    /**
+     * Updates the game based on the game situation.
+     */
+    private void update() {
+
+    }
+
+    /**
+     * Draws the game objects on the screen.
+     */
+    private void draw() {
+
+    }
+
+    /**
+     * Pauses the thread for 17 milliseconds (1000 milliseconds / 60) to achieve frame rate of 60
+     * FPS.
+     */
+    private void sleep() {
 
     }
 }
