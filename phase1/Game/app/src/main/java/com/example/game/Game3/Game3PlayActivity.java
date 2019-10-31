5fd116e23d5ec942d146e4a929f964bf78ee73ac
Chirag Rana <chirag.rana@mail.utoronto.ca>
2019-10-30 20:54:53 -0400

    "Added Game3PlayActivity and corresponding XML file to Git."

---
+++ phase1/Game/app/src/main/java/com/example/game/Game3/Game3PlayActivity.java
@@ -0,0 +1,18 @@
+package com.example.game.Game3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.example.game.R;
+
+public class Game3PlayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game3_view);
+    }
+}
