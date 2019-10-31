7cea7501dfff8691774bfab009c3c2577f7a88e1
Victor <victor.alvarez@mail.utoronto.ca>
2019-10-31 15:30:54 -0400

    "Added 3 Animation instance variables to the RectPlayer class."

--- phase1/Game/app/src/main/java/com/example/game/Game1/RectPlayer.java
+++ phase1/Game/app/src/main/java/com/example/game/Game1/RectPlayer.java
@@ -1,9 +1,13 @@
 package com.example.game.Game1;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.Point;
 import android.graphics.Rect;
+import com.example.game.R;
 
 public class RectPlayer implements GameObject{
 
@@ -16,6 +20,17 @@ public class RectPlayer implements GameObject{
     public RectPlayer(Rect rectangle, int color){
         this.rectangle = rectangle;
         this.color = color;
+
+        BitmapFactory bf = new BitmapFactory();
+        Bitmap idleImg = bf.decodeResource(Constants.CURRENT_CONTEXT.getResources(), R.drawable.alienblue_badge1);
+        Bitmap walk1 = bf.decodeResource(Constants.CURRENT_CONTEXT.getResources(), R.drawable.alienblue_badge1);
+        Bitmap walk2 = bf.decodeResource(Constants.CURRENT_CONTEXT.getResources(), R.drawable.alienblue_badge1);
+
+        idle = new Animation(new Bitmap[]{idleImg}, 2);
+        walkRight = new Animation(new Bitmap[]{walk1, walk2}, 0.5f);
+
+        Matrix m = new Matrix();
+
     }
 
     public Rect getRectangle(){
