aefb74c73cacd1d6726932be089115a9f7134c4a
Chirag Rana <chirag.rana@mail.utoronto.ca>
2019-10-31 20:51:03 -0400

    "Added and partly implemented GameObjectManager class for Game3. Fixed minor bugs in HealthBarObject class and made GameObject class abstract instead of an interface."

--- phase1/Game/app/src/main/java/com/example/game/Game3/Game3Object.java
+++ phase1/Game/app/src/main/java/com/example/game/Game3/Game3Object.java
@@ -3,12 +3,31 @@ package com.example.game.Game3;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 
-/**A Object in Game 3.*/
-public interface Game3Object {
-    int x = 0;
-    int y = 0;
+/**
+ * A Object in Game 3.
+ */
+abstract class Game3Object {
+    private int x;
+    private int y;
 
-    void draw(Canvas canvas, Paint paint);
-    void update();
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    abstract void draw(Canvas canvas, Paint paint);
+
+    abstract void update();
 
 }
