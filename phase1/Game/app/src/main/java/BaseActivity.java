01e07e6e9939524b18b8fc9c17f31051a1cde68a
Kyoji <kyoji.goto@mail.utoronto.ca>
2019-11-01 13:03:28 -0400

    "Created LocaleManager and BaseActivity classes"

---
+++ phase1/Game/app/src/main/java/BaseActivity.java
@@ -0,0 +1,26 @@
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void attachBaseContext(Context base){
+        super.attachBaseContext(base);
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+}
