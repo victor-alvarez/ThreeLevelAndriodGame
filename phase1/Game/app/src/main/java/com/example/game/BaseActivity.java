a67a86a376d694533f3f707d4f85d92a67715704
Kyoji <kyoji.goto@mail.utoronto.ca>
2019-11-02 00:33:53 -0400

    "Documented classes. Translated more strings."

--- phase1/Game/app/src/main/java/com/example/game/BaseActivity.java
+++ phase1/Game/app/src/main/java/com/example/game/BaseActivity.java
@@ -3,40 +3,34 @@ package com.example.game;
 import android.content.Context;
 import android.content.res.Configuration;
 import android.os.Bundle;
-import android.view.MenuItem;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class BaseActivity extends AppCompatActivity {
+    /** Creates a LocaleManager to change language on all activities */
     public static LocaleManager localeManager;
 
+    /** Ensures locale is attached to configuration of context */
     @Override
     protected void attachBaseContext(Context base){
         localeManager = new LocaleManager(base);
         super.attachBaseContext(localeManager.setLocale(base));
     }
 
+    /** Called when activity loads */
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
 
+    /** Called when activity is resumed */
     @Override
     protected void onResume() {
         super.onResume();
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == android.R.id.home) {
-            finish();
-            return true;
-        } else {
-            return super.onOptionsItemSelected(item);
-        }
-    }
-
+    /** Ensures configurations are updated */
     @Override
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
