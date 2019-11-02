e8cb746a583eb09c2bee848c4c1f23d25b42f84d
Victor <victor.alvarez@mail.utoronto.ca>
2019-11-01 22:25:44 -0400

    "Commented out ChangeSongActivity.java, will later be uncommented and completed in Phase 2."

--- phase1/Game/app/src/main/java/com/example/game/Game1/ChangeSongActivity.java
+++ phase1/Game/app/src/main/java/com/example/game/Game1/ChangeSongActivity.java
@@ -1,6 +1,7 @@
+/*
+ * SAVING FOR PHASE 2 COMING SOOOOOOOOON
 package com.example.game.Game1;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -16,9 +17,10 @@ public class ChangeSongActivity extends BaseActivity {
     setContentView(R.layout.activity_change_song);
   }
 
-  /** Called when the user taps the "X" button */
+  /** Called when the user taps the "X" button
   public void toCustomize(View view) {
     Intent intent = new Intent(this, CustomizeActivity.class);
     startActivity(intent);
   }
 }
+*/
