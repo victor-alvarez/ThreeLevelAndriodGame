bcad321378a13978de458335212ce3edc3c35649
Kyoji <kyoji.goto@mail.utoronto.ca>
2019-11-01 21:19:48 -0400

    "Created getter and setter for locale, as well as constructor for LocaleManager"

--- phase1/Game/app/src/main/java/com/example/game/LocaleManager.java
+++ phase1/Game/app/src/main/java/com/example/game/LocaleManager.java
@@ -2,7 +2,11 @@ package com.example.game;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.preference.PreferenceManager;
+
+import java.util.Locale;
 
 public class LocaleManager {
     public static final String ENGLISH = "en";
@@ -15,5 +19,29 @@ public class LocaleManager {
 
     public LocaleManager(Context context) {
         prefs = PreferenceManager.getDefaultSharedPreferences(context);
+    }
+
+    public static Locale getLocale(Resources resources) {
+        Configuration configuration = resources.getConfiguration();
+        return configuration.getLocales().get(0);
+    }
+
+    public Context setLocale(Context context) {
+        return updateResources(context, prefs.getString(LANGUAGE_KEY, ENGLISH));
+    }
+
+    public Context setNewLocale(Context context, String language) {
+        prefs.edit().putString(LANGUAGE_KEY, language).apply();
+        return updateResources(context, language);
+    }
+
+    private Context updateResources(Context context, String language) {
+        Locale locale = new Locale(language);
+        Locale.setDefault(locale);
+        Resources resources = context.getResources();
+        Configuration configuration = new Configuration(resources.getConfiguration());
+        configuration.setLocale(locale);
+        context = context.createConfigurationContext(configuration);
+        return context;
     }
 }
