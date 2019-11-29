15bdad0e4ca7764aca5da740b8c5ee0c4080e37f
Kyoji <kyoji.goto@mail.utoronto.ca>
2019-11-29 16:10:20 -0500

    "Created scoreboard class and a scoreboardData class to store files locally"

--- phase2/Game/app/src/main/java/com/example/game/Scoreboard.java
+++ phase2/Game/app/src/main/java/com/example/game/Scoreboard.java
@@ -1,6 +1,100 @@
 package com.example.game;
 
+import android.content.Context;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
 public class Scoreboard {
-    //TODO: implement a scoreboard system and activity at the end of game 3.
+    private Stack<Pair<Account, Integer>> rank;
+
+    Scoreboard(){
+        rank = new Stack<>();
+    }
+
+    ArrayList<Pair<Account, Integer>> getScoreboard(){
+        Stack<Pair<Account, Integer>> tempStack = new Stack<>();
+        ArrayList<Pair<Account, Integer>> scoreboard = new ArrayList<>();
+        for(int i = 0; i < rank.size(); i++){
+            Pair<Account, Integer> currentAccount = rank.pop();
+            tempStack.push(currentAccount);
+            scoreboard.add(currentAccount);
+        }
+        for(int i = 0; i < tempStack.size(); i++) {
+            Pair<Account, Integer> currentAccount = tempStack.pop();
+            rank.push(currentAccount);
+        }
+        return scoreboard;
+    }
+
+    boolean addScore(Account account, int score, Context context){
+        Pair<Account, Integer> accountIntegerPair = new Pair<>(account, score);
+        int accountRank = findAccountRank(score);
+        if(accountRank != 0) {
+            placeAccount(accountIntegerPair, accountRank);
+            SaveScoreboardData.save( context, this);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private void placeAccount(Pair<Account, Integer> accountIntegerPair, int accountRank) {
+        Stack<Pair<Account, Integer>> tempStack = new Stack<>();
+        while (tempStack.size() != 5){
+            Pair<Account, Integer> currentAccount = rank.pop();
+            if(accountRank == tempStack.size() - 1){
+                tempStack.push(accountIntegerPair);
+            }else{
+                tempStack.push(currentAccount);
+            }
+        }
+        for(int i = 0; i < tempStack.size(); i++){
+            Pair<Account, Integer> currentAccount= tempStack.pop();
+            rank.push(currentAccount);
+        }
+    }
+
+    private int findAccountRank(int score) {
+        if(rank.isEmpty()){
+            return 1;
+        } else if(isScoreHighEnough(score)){
+            int count = 0;
+            Stack<Pair<Account, Integer>> tempStack = new Stack<>();
+            for(int i = 0; i < rank.size(); i++){
+                Pair<Account, Integer> currentAccount = rank.pop();
+                if(currentAccount.getSecond() < score){
+                    count++;
+                }
+                tempStack.push(currentAccount);
+            }
+            for(int i = 0; i < tempStack.size(); i++){
+                Pair<Account, Integer> currentAccount= tempStack.pop();
+                rank.push(currentAccount);
+            }
+            return 6 - count; //size of scoreboard + 1
+        }else{
+            //score is not high enough to be in the scoreBoard
+            return 0;
+        }
+    }
+
+    private boolean isScoreHighEnough(int score) {
+        boolean isScoreHighEnough = false;
+        Stack<Pair<Account, Integer>> tempStack = new Stack<>();
+        for(int i = 0; i < rank.size(); i++){
+            Pair<Account, Integer> currentAccount= rank.pop();
+            if(currentAccount.getSecond() < score){
+                isScoreHighEnough = true;
+            }
+            tempStack.push(currentAccount);
+        }
+        for(int i = 0; i < tempStack.size(); i++){
+            Pair<Account, Integer> currentAccount= tempStack.pop();
+            rank.push(currentAccount);
+        }
+        return isScoreHighEnough;
+    }
+
 
 }
