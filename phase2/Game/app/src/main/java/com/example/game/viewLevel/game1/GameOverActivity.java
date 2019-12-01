ab71c79facc8dc826cd5abea9cc0cda35d3bc265
Clifton <clifton.sahota@mail.utoronto.ca>
2019-12-01 15:51:54 -0500

    "Bugfix related to resuming game."

--- phase2/Game/app/src/main/java/com/example/game/viewLevel/game1/GameOverActivity.java
+++ phase2/Game/app/src/main/java/com/example/game/viewLevel/game1/GameOverActivity.java
@@ -70,7 +70,11 @@ public class GameOverActivity extends BaseActivity {
      */
     public void retry(View view) {
         Intent intent = new Intent(this, BallJumperActivity.class);
-        intent.putExtra("difficulty", getIntent().getStringExtra("difficulty"));
+        String difficultyLevel = getIntent().getStringExtra("difficulty");
+        if (difficultyLevel == null){
+            difficultyLevel = "normal";
+        }
+        intent.putExtra("difficulty", difficultyLevel);
         account.decrementLevel(getApplicationContext().getFilesDir());
         startActivity(intent);
     }
