6e5963420e44d9654ec96dcebef14374b600afcd
Chirag Rana <chirag.rana@mail.utoronto.ca>
2019-12-01 15:32:56 -0500

    Updated Game 3 Strategies to make the game harder to win.

--- phase2/Game/app/src/main/java/com/example/game/models/game3/Game3NormalStrategy.java
+++ phase2/Game/app/src/main/java/com/example/game/models/game3/Game3NormalStrategy.java
@@ -3,25 +3,44 @@ package com.example.game.models.game3;
 import java.util.Random;
 
 class Game3NormalStrategy implements Game3Strategy {
-    private int[] enemyAttack = {9, 9, 10, 10, 10, 12, 12, 12, 13, 13, 13, 15};
+    private int[] enemyHigherAttack = {11, 11, 11, 12, 12, 13, 13, 15};
+    private int[] enemyLowerAttack = {6, 7, 7, 7, 8, 8, 9, 9};
 
-    private int[] enemyDefend = {9, 9, 10, 10, 10, 12, 12, 12, 13, 13, 13, 15};
 
     @Override
     public int enemyAttack() {
-        int damageIndex = new Random().nextInt(enemyAttack.length);
-        return enemyAttack[damageIndex];
+        // 80% chance that higher attack will be picked.
+        int attack = new Random().nextInt(10);
+        if (attack <= 7){
+            int damageIndex = new Random().nextInt(enemyHigherAttack.length);
+            return enemyHigherAttack[damageIndex];
+        } else {
+            int damageIndex = new Random().nextInt(enemyLowerAttack.length);
+            return enemyLowerAttack[damageIndex];
+        }
     }
 
     @Override
     public int enemyDefend() {
-        int damageIndex = new Random().nextInt(enemyDefend.length);
-        return enemyDefend[damageIndex];
+
+        int attack = new Random().nextInt(4);
+        if (attack == 0){
+            int damageIndex = new Random().nextInt(enemyHigherAttack.length);
+            return enemyHigherAttack[damageIndex];
+        } else {
+            int damageIndex = new Random().nextInt(enemyLowerAttack.length);
+            return enemyLowerAttack[damageIndex];
+        }
     }
 
     @Override
     public int playerAttack(BottleObject healthPotion) {
-        return 12;
+        int randomVar = new Random().nextInt(4);
+        if (randomVar == 0){
+            healthPotion.setActive(true);
+        }
+        return 10;
+
     }
 
     @Override
