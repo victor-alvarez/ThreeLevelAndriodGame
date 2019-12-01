7752724da4f3dd7a574feb635380245889e09d37
Clifton <clifton.sahota@mail.utoronto.ca>
2019-12-01 15:46:55 -0500

    "Main menu follows MVP and resume game works properly now."

---
+++ phase2/Game/app/src/main/java/com/example/game/models/MainLoadUseCases.java
@@ -0,0 +1,64 @@
+package com.example.game.models;
+
+import java.io.File;
+
+import static com.example.game.BaseActivity.account;
+
+public class MainLoadUseCases {
+    /**
+     * Performs actions related to starting the first game
+     * @param contextFile the file the database is saved at
+     */
+    public void game1(LoadGameListener listener, File contextFile) {
+        account.resetValues(contextFile);
+        listener.directToGame1();
+    }
+
+    /**
+     * Performs actions related to starting the second game
+     * @param contextFile the file the database is saved at
+     */
+    public void game2(LoadGameListener listener, File contextFile) {
+        while (account.getLastAttemptedLevel() != 2){
+            account.incrementLevel(contextFile);
+        }
+        listener.directToGame2();
+    }
+
+    /**
+     * Performs actions related to starting the third game
+     * @param contextFile the file the database is saved at
+     */
+    public void game3(LoadGameListener listener, File contextFile) {
+        while (account.getLastAttemptedLevel() != 3){
+            account.incrementLevel(contextFile);
+        }
+        listener.directToGame3();
+    }
+
+    /**
+     * Performs actions related to resuming the game
+     * @param contextFile the file the database is saved at
+     */
+    public void resume(LoadGameListener listener, File contextFile){
+        int level = account.getLastAttemptedLevel();
+
+        switch (level){
+            case 0:
+                listener.directToGame1();
+                break;
+            case 1:
+                listener.directToAfterGame1();
+                break;
+            case 2:
+                listener.directToGame2();
+                break;
+            case 3:
+                listener.directToGame3();
+                break;
+            case 4:
+                listener.directToAfterGame3();
+                break;
+        }
+    }
+}
